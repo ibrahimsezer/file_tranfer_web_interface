0ba87ae810f1ec1cc10af35a33e34a43ed90f0ef
İbrahim SEZER <ibrahimszr16@gmail.com>
2025-01-05 14:52:18 +0300

    update website

--- client/src/config.js
+++ client/src/config.js
@@ -1,10 +1,4 @@
-  export const API_URL = 'http://localhost:5000'
-
-  /*
-
   export const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://secure-file-tranfer.onrender.com'
-  : 'http://localhost:5000'; 
-  
+  ? 'https://file-tranfer-web-interface.onrender.com'
+  : 'http://localhost:5000';
 
-  */
